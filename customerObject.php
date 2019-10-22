c81aee4e72747c39ec29a54995ca0604d4d19109
ashval20 <namithaodackal1996@gmail.com>
2019-10-23 12:46:32 +1300

    customer class
    here customer class is created with all of its objests like id,name,password,etc.

---
+++ customerObject.php
@@ -0,0 +1,62 @@
+<?php
+class Customer {
+  private $customerid;
+  private $customerName;
+  private $username;
+  private $password;
+  private $email;
+
+  //function for constructing the objects
+  public function __construct($customerName, $username, $password, $email){
+    $this->customerName = $customerName;
+    $this->username = $username;
+    $this->password = $password;
+    $this->email = $email;
+  }
+
+   public function __destruct(){
+
+   }
+
+  public function saveUser(){
+    $db = mysqli_connect('localhost', 'root', '', 'safetrade');
+    $query = "INSERT INTO customers (customerName, username, password, email) VALUES('$this->customerName', '$this->username', '$this->password', '$this->email')";
+  	mysqli_query($db, $query);
+  }
+
+  public function setValues($username){
+    $db = mysqli_connect('localhost', 'root', '', 'safetrade');
+    $query = "SELECT customerid FROM customers WHERE username == '$username'" ;
+    $result = mysqli_query($db, $query);
+    $this->customerid = $result;
+    $query = "SELECT customerName FROM customers WHERE username =='$username'" ;
+    $result = mysqli_query($db, $query);
+    $this->customerName = $result;
+    $query = "SELECT username FROM customers WHERE username == '$username'" ;
+    $result = mysqli_query($db, $query);
+    $this->username = $result;
+    $query = "SELECT password FROM customers WHERE username == '$username'" ;
+    $result = mysqli_query($db, $query);
+    $this->password = $result;
+    $query = "SELECT email FROM customers WHERE username == '$username'" ;
+    $result = mysqli_query($db, $query);
+    $this->email = $result;
+  }
+
+  public function getID(){
+    return $this->customerid;
+  }
+  public function getName(){
+    return $this->customerName;
+  }
+  public function getUserName(){
+    return $this->username;
+  }
+  public function getPassword(){
+    return $this->password;
+  }
+  public function getEmail(){
+    return $this->email;
+  }
+}
+?>
