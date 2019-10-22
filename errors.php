23911255e1a3229094336584e91092246744ade7
ashval20 <namithaodackal1996@gmail.com>
2019-10-23 12:46:59 +1300

    error form
    error messages for all the pages

---
+++ errors.php
@@ -0,0 +1,9 @@
+<?php
+// produces list of contextual error messages for all the forms
+if (count($errors) > 0) : ?>
+  <div class="error">
+  	<?php foreach ($errors as $error) : ?>
+  	  <p><?php echo $error ?></p>
+  	<?php endforeach ?>
+  </div>
+<?php  endif ?>
