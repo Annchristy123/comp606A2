6f3233f5bc220d107b341ce3fd29c127203254a9
ashval20 <namithaodackal1996@gmail.com>
2019-10-30 17:30:25 +1300

    find customer page
    finding the customer details

---
+++ findCustomer.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("headers.php");
+
+//$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
+$newUser = Customer::find($db, $_POST['email'], $_POST['password']);
+//var_dump($newUser);
+if(!$newUser){
+    echo "<h2>Failed</h2>";
+}
+ else {
+    echo "<h2>success</h2>";
+    
+}
+
+
+require_once("footer.php");
+
+?>
