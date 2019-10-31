09f332966be23df6377005b447b1b0a27f7f2f78
annchristy1993@gmail.com <annchristy1993@gmail.com>
2019-11-01 01:50:23 +1300

    tradesman
    add tradesman

---
+++ addTradesman.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("headers.php");
+
+//$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
+$newUser = Tradesman::create($db, $_POST['fname'], $_POST['lname'], $_POST['phone'], $_POST['email'], $_POST['password']);
+
+echo "hi2";
+
+var_dump($newUser);
+if(!$newUser){
+    echo "<h2>Failed</h2>";
+}
+ else {
+    $_SESSION['tid'] = $newUser->getTId();
+    header("Location: viewjobtrades.php?tid=".$_SESSION['tid']);//redirecting to user profile  
+
+    
+}
+
+
+require_once("footer.php");
+
+?>
