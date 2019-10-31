bcb003ab37990df8ffc08fca4607a25e57e78cd7
annchristy1993@gmail.com <annchristy1993@gmail.com>
2019-11-01 01:52:47 +1300

    customer login

---
+++ customerslogin.php
@@ -0,0 +1,106 @@
+
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+	font-size: 100%;
+	background-image: url("massage.jpg");
+	background-repeat: no-repeat;
+    background-size: cover;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: right;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+input{
+    width: 450px;
+	margin: 0 auto;
+    padding: 10px;
+    border: 1px solid #ccc;
+}
+.main-block {
+    border-bottom: 8px;
+    min-height: 100px;
+}
+
+#main-border{
+	border-radius : 10px;
+	background-color: white;
+	border: 3px solid #133783;
+	width: 500px;
+	margin: 0 auto;
+}
+.main-form{
+	width:450px;
+	margin:0 auto;
+}
+.login_button {
+    background-color: #4CAF50;
+    color: white;
+    margin-top:10px;
+    margin-bottom: 10px;
+    padding:10px;
+    width: 100%;
+	font-size:18px;
+	font-weight: bold;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="" href="tradesmanlogin.php">Tradesman Login</a>
+  <a class="active" href="customerslogin.php">Customer Login</a>
+
+</div>
+
+<div class="main-block">
+</div>
+	<div id="main-border">
+    <p><center><img src="logo.jpg" alt="Logo" width="100" height="100"></p></center>
+
+	<center><h2>Customer Login</h2></center>
+			
+		<form action="findCustomer.php" method="post">
+		
+			<div class="main-form">
+				<input type="text" placeholder="Enter email" name="email" required>
+				<input type="password" placeholder="Enter Password" name="password" required>
+				<button class="login_button" name="login" type="submit">Login</button>
+        <a href="custForm.php" >Create Customer</a>
+        <a href="customerlogin.php" ><<- back to home</a>
+			</div>
+		</form>
+		
+	</div>
+
+</body>
+</html>
