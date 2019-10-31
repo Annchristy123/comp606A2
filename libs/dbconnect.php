3407c0bd06f8c31675cfddc0745bcb43eb88b933
annchristy1993@gmail.com <annchristy1993@gmail.com>
2019-11-01 01:54:18 +1300

    db connection

---
+++ libs/dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+
+error_reporting(0);
+
+ 
+$mysql_host='localhost';
+$mysql_user='root';
+$mysql_password='';
+$database='safetrade';
+
+$db=mysqli_connect($mysql_host,$mysql_user,$mysql_password,$database);
+if ($db == false){
+    header("location: sitedown.php");
+  }
+
+?>
