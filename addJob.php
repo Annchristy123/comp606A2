56a077b98ca8b46db0f6a5c25658c3def63aea2a
ashval20 <namithaodackal1996@gmail.com>
2019-11-02 00:04:28 +1300

    add job
    posting jobs to the database when customer add jobs

--- addJob.php
+++ addJob.php
@@ -3,16 +3,17 @@
 require_once("headers.php");
 
 //$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
-$newUser = Job::create($db, $_POST['Jobtype'], $_POST['Description'], $_POST['Location'], $_POST['Startdate'], $_POST['Estimatedate'],$_POST['Expectedcost'],);
-
+$newUser = Job::create($db, $_POST['Jobtype'], $_POST['Description'], $_POST['Location'], $_POST['Startdate'], $_POST['Estimatedate'],$_POST['Expectedcost']);
 
 if(!$newUser){
     echo "<h2>Failed</h2>";
 }
- else {
-    echo "<h2>New User Created</h2>";
-    echo "<h2>Login Success</h2>";
+ else 
+ {
+
     $_SESSION['jid'] = $newUser->getJId();
+    header("Location: custhome.php?cid=".$_SESSION['cid']);//redirecting to user profile  
+
     
 }
 
