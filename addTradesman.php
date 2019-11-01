e335e34fe79d9b03234f5453ef29895f035c4c82
ashval20 <namithaodackal1996@gmail.com>
2019-11-02 00:05:09 +1300

    add tradesman
    adding tradesman details to the database

--- addTradesman.php
+++ addTradesman.php
@@ -1,12 +1,7 @@
 <?php
 
 require_once("headers.php");
-echo "hi1";
-echo $_POST['fname'];
-echo $_POST['lname'];
-echo $_POST['phone'];
-echo $_POST['email'];
-echo $_POST['password'];
+
 //$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
 $newUser = Tradesman::create($db, $_POST['fname'], $_POST['lname'], $_POST['phone'], $_POST['email'], $_POST['password']);
 
@@ -17,9 +12,9 @@ if(!$newUser){
     echo "<h2>Failed</h2>";
 }
  else {
-    echo "<h2>New User Created</h2>";
-    echo "<h2>Login Success</h2>";
     $_SESSION['tid'] = $newUser->getTId();
+    header("Location: viewjobtrades.php?tid=".$_SESSION['tid']);//redirecting to user profile  
+
     
 }
 
