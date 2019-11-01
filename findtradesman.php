a7c4da57545b973b0d8c9eae1e91af040c683303
ashval20 <namithaodackal1996@gmail.com>
2019-11-02 00:12:44 +1300

    tradesman finding
    to find tradesman when he logged in

--- findtradesman.php
+++ findtradesman.php
@@ -4,12 +4,14 @@ require_once("headers.php");
 
 //$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
 $newUser = Tradesman::find($db, $_POST['email'], $_POST['password']);
+//echo $_SESSION['tid'];
 //var_dump($newUser);
 if(!$newUser){
     echo "<h2>Failed</h2>";
 }
  else {
-    echo "<h2>success</h2>";
+    header("Location: viewjobtrades.php?tid=".$_SESSION['tid']);//redirecting to user profile  
+
     
 }
 
