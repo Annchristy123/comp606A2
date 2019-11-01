5ada9d31a3cb24e3535a54a08c02d595f1e73b39
ashval20 <namithaodackal1996@gmail.com>
2019-11-02 00:02:44 +1300

    add estimate page

---
+++ addEstimate.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("headers.php");
+
+//$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
+$newEstimate = Estimate::create($db,$_POST['jid'],$_SESSION['tid'], $_POST['LabourCost'], $_POST['MaterialCost'], $_POST['ExpirationDate']);
+var_dump($newEstimate);
+
+if(!$newEstimate){
+    echo "<h2>Failed</h2>";
+}
+ else {
+   
+    echo "<h2>Estimte Posted Success</h2>";
+    $_SESSION['eid'] = $newEstimate->getEId();
+    
+}
+
+
+require_once("footer.php");
+
+?>
---
+++ estimateForm.php
@@ -0,0 +1,107 @@
+
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+	font-size: 100%;
+	background-image: url("massage.jpg");
+	background-repeat: no-repeat;
+    background-size: cover;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: right;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+input{
+    width: 450px;
+	margin: 0 auto;
+    padding: 10px;
+    border: 1px solid #ccc;
+}
+.main-block {
+    border-bottom: 8px;
+    min-height: 100px;
+}
+
+#main-border{
+	border-radius : 10px;
+	background-color: white;
+	border: 3px solid #133783;
+	width: 500px;
+	margin: 0 auto;
+}
+.main-form{
+	width:450px;
+	margin:0 auto;
+}
+.login_button {
+    background-color: #4CAF50;
+    color: white;
+    margin-top:10px;
+    margin-bottom: 10px;
+    padding:10px;
+    width: 100%;
+	font-size:18px;
+	font-weight: bold;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="" href="tradesmanlogin.php">Tradesman Logout</a>
+  <a class="" href="viewestimate.php">View Estimate</a>
+  <a class="" href="viewjobtrades.php"> View Jobs</a>
+
+</div>
+
+<div class="main-block">
+</div>
+	<div id="main-border">
+    <p><center><img src="logo.jpg" alt="Logo" width="100" height="100"></p></center>
+
+  	<center><h2>Estimate Form</h2></center>
+			
+		<form action="addEstimate.php" method="post">
+		
+			<div class="main-form">
+              <input type="text" placeholder="Labour Cost" name="LabourCost" required>
+              <input type="text" placeholder="Material Cost" name="MaterialCost">
+              <input type="date" placeholder="ExpirationDate" name="ExpirationDate" >
+              <button class="login_button" type="submit">Post Estimate</button>
+              <input type="hidden" name="jid" value="<?php echo $_GET['jid']; ?>" >
+			</div>
+		</form>
+		
+	</div>
+
+</body>
+</html>
