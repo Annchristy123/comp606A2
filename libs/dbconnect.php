bfa61456aa88d4dc1c4134c02014f5513bd3b66f
ashval20 <namithaodackal1996@gmail.com>
2019-11-04 19:28:23 +1300

    database connectivity
    establish connection to the database

--- libs/dbconnect.php
+++ libs/dbconnect.php
@@ -2,15 +2,13 @@
 
 error_reporting(0);
 
- 
-$mysql_host='localhost';
-$mysql_user='root';
-$mysql_password='';
-$database='safetrade';
 
-$db=mysqli_connect($mysql_host,$mysql_user,$mysql_password,$database);
-if ($db == false){
-    header("location: sitedown.php");
-  }
+$mysql_host = 'localhost';
+$mysql_user = 'root';
+$mysql_password = '';
+$database = 'safetrade';
 
-?>
+$db = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $database);
+if ($db == false) {
+  header("location: sitedown.php");
+}
