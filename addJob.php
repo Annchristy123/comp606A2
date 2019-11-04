6a1cc017e7bf5cbd60f93ba70a5aaa78031ab305
ashval20 <namithaodackal1996@gmail.com>
2019-11-04 19:15:45 +1300

    add job page updated

--- addJob.php
+++ addJob.php
@@ -4,20 +4,16 @@ require_once("headers.php");
 
 //$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
 
-
 $newUser = Job::create($db, $_POST['Jobtype'], $_POST['Description'], $_POST['Location'], $_POST['Startdate'], $_POST['Estimatedate'],$_POST['Expectedcost']);
-
 
 if(!$newUser){
     echo "<h2>Failed</h2>";
 }
-
  else 
  {
 
     $_SESSION['jid'] = $newUser->getJId();
     header("Location: custhome.php?cid=".$_SESSION['cid']);//redirecting to user profile  
-
 
     
 }
