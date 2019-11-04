caac0c45800979efd999de3ebf42ad3f5a30d090
ashval20 <namithaodackal1996@gmail.com>
2019-11-04 19:15:19 +1300

    add estimate page updated

--- addEstimate.php
+++ addEstimate.php
@@ -4,7 +4,6 @@ require_once("headers.php");
 
 //$user=new Customer(1,$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['email'],$_POST['phone'],$_POST['password']);
 $newEstimate = Estimate::create($db,$_POST['jid'],$_SESSION['tid'], $_POST['LabourCost'], $_POST['MaterialCost'], $_POST['ExpirationDate']);
-var_dump($newEstimate);
 
 if(!$newEstimate){
     echo "<h2>Failed</h2>";
@@ -13,6 +12,8 @@ if(!$newEstimate){
    
     echo "<h2>Estimte Posted Success</h2>";
     $_SESSION['eid'] = $newEstimate->getEId();
+    header("Location: viewjobtrades.php?tid=".$_SESSION['tid']);//redirecting to user profile  
+
     
 }
 
