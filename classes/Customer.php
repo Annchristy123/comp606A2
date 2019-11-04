e59b1a65f5b96d3d8b3a1e76c9ef79fb53c8e68f
ashval20 <namithaodackal1996@gmail.com>
2019-11-04 19:17:04 +1300

    customer class

--- classes/Customer.php
+++ classes/Customer.php
@@ -1,6 +1,7 @@
 <?php
 
-class Customer{
+class Customer
+{
 
   // private properties of this class 
   private $cid = null;
@@ -9,10 +10,11 @@ class Customer{
   private $email = "";
   private $phone = null;
   private $password = null;
-  
-  
+
+
   // constructor to create new user object
-  public function __construct($cid, $fname, $lname,$phone,$email, $password){
+  public function __construct($cid, $fname, $lname, $phone, $email, $password)
+  {
     $this->cid = $cid;
     $this->fname = $fname;
     $this->lname = $lname;
@@ -21,135 +23,148 @@ class Customer{
     $this->password = $password;
   }
 
-  public static function create($db, $fname, $lname,$phone,$email, $password){
+  public static function create($db, $fname, $lname, $phone, $email, $password)
+  {
     // create a new user record in customerdetails table and if successful 
     // create a user object and return it otherwise return false;   
-    $password=md5($password);//Password encryption
+    $password = md5($password); //Password encryption
     $result = false;
-    var_dump(result);
-      $sql = sprintf("insert into customerdetails(FirstName, LastName,Phone,Email,Password) values('%s', '%s', '%s','%s', '%s')",  $fname, $lname, $phone,$email, $password);
-      var_dump(sql);
-      $qresult = $db->query($sql);
-      if ($qresult){
+    $sql = sprintf("insert into customerdetails(FirstName, LastName,Phone,Email,Password) values('%s', '%s', '%s','%s', '%s')",  $fname, $lname, $phone, $email, $password);
+    $qresult = $db->query($sql);
+    print_r($qresult);
+    if ($qresult) {
+      print_r($qresult);
       $cid = $db->insert_id;
-      $user = new Customer($cid, $fname, $lname,$phone,$email, $password);      
+      $user = new Customer($cid, $fname, $lname, $phone, $email, $password);
       $result = $user;
-      $_SESSION['username']=$email;//initialising session
-        $_SESSION['firstname']=$fname;
-        $_SESSION['lastname']=$lname;
-        $_SESSION['tid']=$cid;
-     }    
+      $_SESSION['username'] = $email; //initialising session
+      $_SESSION['firstname'] = $fname;
+      $_SESSION['lastname'] = $lname;
+      $_SESSION['tid'] = $cid;
+    } else {
+      echo "Error: " . $sql . "" . mysqli_error($db);
+    }
     return $result;
   }
-  public static function find($db, $email,$password){
+  public static function find($db, $email, $password)
+  {
     // search customerdetails table and locate record with id
     // get that record and create user object 
     // return user object OR false if we cannot find it
     $result = false;
-    $password=md5($password);
-    $sql = sprintf("select * from customerdetails where Email='%s' and Password='%s'", $email,$password);
+    $password = md5($password);
+    $sql = sprintf("select * from customerdetails where Email='%s' and Password='%s'", $email, $password);
     //var_dump($sql);
     $qresult = $db->query($sql);
-    if ($qresult){
-      if ($qresult->num_rows == 1){
+    if ($qresult) {
+      if ($qresult->num_rows == 1) {
         $row = $qresult->fetch_assoc();
-        $user = new Customer($row['Cid'], $row['FirstName'], $row['LastName'], $row['Phone'],$row['Email'], $row['Password']);
-        $_SESSION['username']=$email;//initialising session
-         $_SESSION['cid']=$row['Cid'];
-
-         $_SESSION['lastname']=$row['LastName'];
+        $user = new Customer($row['Cid'], $row['FirstName'], $row['LastName'], $row['Phone'], $row['Email'], $row['Password']);
+        $_SESSION['username'] = $email; //initialising session
+        $_SESSION['cid'] = $row['Cid'];
+        $_SESSION['lastname'] = $row['LastName'];
         $result = $user;
       }
     }
     return $result;
-  } 
+  }
 
-  
+
 
   // ------ setter methods -------
-  public function setCId($cid){
+  public function setCId($cid)
+  {
     $this->$cid = $cid;
   }
 
-  public function setFName($fname){
+  public function setFName($fname)
+  {
     $result = true;
-    if (is_string($fname)){
+    if (is_string($fname)) {
       $this->fname = $fname;
     } else $result = false;
     return $result;
   }
 
-  public function setLName($lname){
+  public function setLName($lname)
+  {
     $result = true;
-    if (is_string($lname)){
+    if (is_string($lname)) {
       $this->lname = $lname;
     } else $result = false;
     return $result;
   }
- 
 
-     public function setPhone($phone){
+
+  public function setPhone($phone)
+  {
     $result = true;
-    if (preg_match('/^[0-9]{10}+$/', $phone)){
+    if (preg_match('/^[0-9]{10}+$/', $phone)) {
       $this->phone = $phone;
       $result = true;
-    } 
-    else {
-      $result=false;
+    } else {
+      $result = false;
     }
     return $result;
   }
- 
 
-  public function setEmail($email){
+
+  public function setEmail($email)
+  {
     // string, email format
     $result = true;
-    if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){
+    if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)) {
       $result = false;
     } else {
       $this->email = $email;
     }
     return $result;
-  }  
+  }
 
-  public function setPassword($password){
+  public function setPassword($password)
+  {
     // string, email format
     $result = true;
     $uppercase = preg_match('@[A-Z]@', $password);
     $lowercase = preg_match('@[a-z]@', $password);
     $number    = preg_match('@[0-9]@', $password);
     $specialChars = preg_match('@[^\w]@', $password);
-    
-    if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+
+    if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
       $result = false;
     } else {
       $this->password = $password;
     }
     return $result;
-  } 
+  }
 
   // ------- getter methods ----------
-  public function getFName(){    
+  public function getFName()
+  {
     return $this->fname;
   }
 
-  public function getLName(){    
+  public function getLName()
+  {
     return $this->lname;
   }
-  public function getCId(){
+  public function getCId()
+  {
     return $this->cid;
   }
 
-  public function getEmail(){
+  public function getEmail()
+  {
     return $this->email;
   }
 
-  public function getPhone(){
+  public function getPhone()
+  {
     return $this->phone;
   }
 
-  public function getPassword(){
+  public function getPassword()
+  {
     return $this->password;
   }
-  
 }
