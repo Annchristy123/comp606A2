313d33a6ced89b8a9bb8753b15442ec52bdb21cd
ashval20 <namithaodackal1996@gmail.com>
2019-11-04 19:17:30 +1300

    estimate class

--- classes/Estimate.php
+++ classes/Estimate.php
@@ -1,3 +1,4 @@
+
 <!-- Class Estimate having functions to update ,edit and delete estimates as well fetch estimates based on conditions -->
 <?php
 
@@ -6,7 +7,7 @@ class Estimate{
   // private properties of this class 
   private $eid = null;
   private $jid = null;
-
+  //private $cid = null;
   private $tid = null;
   private $mcost = "";
   private $lcost = "";
@@ -16,13 +17,13 @@ class Estimate{
   
   
   // constructor to create new estimate object
-
-  public function __construct($eid, $jid,$tid,$mcost, $lcost, $tcost, $expdate,$isaccepted){
+  public function __construct($eid, $jid,$tid, $lcost,$mcost, $tcost, $expdate,$isaccepted){
     $this->eid = $eid;
     $this->jid = $jid;
+    //$this->cid = $cid;
     $this->tid = $tid;
-    $this->mcost = $mcost;
     $this->lcost = $lcost;
+    $this->mcost = $mcost;
     $this->tcost = $tcost;
     $this->expdate = $expdate;
     $this->isaccepted=$isaccepted;
@@ -36,23 +37,70 @@ class Estimate{
     $tcost=$lcost+$mcost;
     //echo $tcost;
     $isaccepted=0;
-
     $sql = sprintf("insert into estimatedetails(Tid,Jid,LabourCost,MaterialCost, TotalCost, ExpirationDate,IsAccepted) values('%s', '%s','%s', '%s', '%s', '%s','%s')",  $tid,$jid,$lcost,$mcost ,$tcost, $expdate,$isaccepted);
-
-    //echo $sql;
+    echo $sql;
     $qresult = $db->query($sql);
     if ($qresult){
-      $eid = $db->insert_id;
+     $eid = $db->insert_id;
       $estimate = new Estimate($eid,$jid,$tid, $lcost,$mcost, $tcost, $expdate,$isaccepted);
-
       $result = $estimate;
+      //var_dump($result);
     }
     return $result;
   }
-  public static function getAll($db){
+  public static function updateestimate($db,$eid){
+    // create a new estimate record in estimatedetails table and if successful 
+    // create a estimate object and return it otherwise return false;
+    echo $eid;
+    $sql = sprintf("update estimatedetails set IsAccepted=1 where Eid='%s'", $eid);
+
+    echo $sql;
+    $qresult = $db->query($sql);
+    if ($qresult){
+      $result = true;
+     
+    }
+    echo $result;
+    return $result;
+  }
+
+  public static function getEstimateById($db,$eid){
+  $result = false;
+    $sql = sprintf("select * from estimatedetails where eid=%s ", $eid);
+    //echo $sql;
+    $result = $db->query($sql);    
+    $estimate = false;
+    if ($result){
+     
+      while($row = $result->fetch_assoc()){
+        $estimate =  new Estimate($row['Eid'],$row['Jid'],$row['Tid'], $row['LabourCost'], $row['MaterialCost'], $row['TotalCost'], $row['ExpirationDate'], $row['IsAccepted']);
+        
+      }    
+    }
+    return $estimate; 
+  }
+  public static function getAll($db,$tid){
     // get all jobs and return as a collection of job objects
     // returns false or a collection of job objects
-    $sql = "select * from estimatedetails";
+    $result = false;
+    $sql = sprintf("select * from estimatedetails where tid=%s ", $tid);
+    //echo $sql;
+    $result = $db->query($sql);    
+    $estimate = false;
+    if ($result){
+      $estimates = new Collection();
+      while($row = $result->fetch_assoc()){
+        $estimate =  new Estimate($row['Eid'],$row['Jid'],$row['Tid'], $row['LabourCost'], $row['MaterialCost'], $row['TotalCost'], $row['ExpirationDate'], $row['IsAccepted']);
+        $estimates->Add($row['Eid'], $estimate);  
+        //echo $row['Jid']    ;
+      }    
+    }
+    return $estimates; 
+  }
+  public static function find($db,$jid){
+    // get all jobs and return as a collection of job objects
+    // returns false or a collection of job objects
+    $sql = sprintf("select * from estimatedetails where Jid=%s",$jid);
     $result = $db->query($sql);    
     //echo $sql;
     $estimate = false;
@@ -123,6 +171,10 @@ class Estimate{
     return $this->lcost;
   }
 
+  public function getJId(){
+    return $this->jid;
+  }
+
   public function getTcost(){
     return $this->tcost;
   }
@@ -136,5 +188,3 @@ class Estimate{
   }
   
 }
-?>
-
